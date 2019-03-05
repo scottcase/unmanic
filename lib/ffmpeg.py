be6fc39dcc8462e4f86690217dc9229a121fdeba
scottcase <scott.case.1@gmail.com>
2019-03-05 15:14:14 -0500

    send request to plex to scan library

--- lib/ffmpeg.py
+++ lib/ffmpeg.py
@@ -314,11 +314,14 @@ class FFMPEGHandle(object):
         # If file conversion was successful, we will get here
         #url = 'http://10.0.0.14:3467/d700e4b05d6d4989b49eac4207cf4b05'
         #payload = {'eventType': 'Manual', 'filepath': destPath}
+        url = ' http://10.0.0.25:32400/library/sections/44/refresh'
+        payload = {'X-Plex-Token': self.settings.PLEX_TOKEN}
         #r = requests.post(url, data=payload)
-        #if r.status_code == 200:
-        #    self._log("Successfully send request to plex_autoscan")
-        #else:
-        #    self._log("Did not Successfully send request to plex_autoscan: URL:{} payload:{} status_code:{}".format(url,payload,r.status_code))
+        r = requests.get(url, data=payload)
+        if r.status_code == 200:
+            self._log("Successfully send request to plex_autoscan")
+        else:
+            self._log("Did not Successfully send request to plex_autoscan: URL:{} payload:{} status_code:{}".format(url,payload,r.status_code))
         self._log("Successfully processed file '{}'".format(srcPath))
         return True
 
