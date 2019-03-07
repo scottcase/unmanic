31697cfdd340ac4d66c14db81133af080d6cca59
scottcase <scott.case.1@gmail.com>
2019-03-07 12:11:18 -0500

    Plex Notify

--- lib/ffmpeg.py
+++ lib/ffmpeg.py
@@ -313,11 +313,12 @@ class FFMPEGHandle(object):
             return False
         # If file conversion was successful, we will get here
         # url = 'http://10.0.0.14:3467/d700e4b05d6d4989b49eac4207cf4b05'
-        # payload = {'eventType': 'Manual', 'filepath': destPath}
-        # r = requests.post(url, data=payload)
         url = self.settings.PLEX_URL
-        payload = {'X-Plex-Token': self.settings.PLEX_TOKEN}
-        r = requests.get(url, params=payload)
+        payload = {'eventType': 'Manual', 'filepath': destPath}
+        r = requests.post(url, data=payload)
+        # url = self.settings.PLEX_URL
+        # payload = {'X-Plex-Token': self.settings.PLEX_TOKEN}
+        # r = requests.get(url, params=payload)
         if r.status_code == 200:
             self._log("Successfully send request to plex_autoscan")
         else:
